fb62aa4c759a6d9c9a245ccc77f569c383dba25b
Frank-Mayer <git@frank-mayer.io>
2023-05-28 02:31:40 +0200

    fixed error printing issue with tab

--- src/main/java/io/frankmayer/Error.java
+++ src/main/java/io/frankmayer/Error.java
@@ -1,5 +1,10 @@
 package io.frankmayer;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Error {
 
   public static void panic(final String message) {
@@ -8,51 +13,68 @@ public class Error {
   }
 
   public static void panic(final String message, final Throwable cause) {
-    System.err.println(Error.prettyPrint(message, cause.getMessage()));
+    final var sw = new StringWriter();
+    final var pw = new PrintWriter(sw);
+    cause.printStackTrace(pw);
+    System.err.println(Error.prettyPrint(message, cause.getMessage(), sw.toString()));
     System.exit(message.hashCode());
   }
 
   public static void panic(final Throwable cause) {
-    System.err.println(Error.prettyPrint(cause.getMessage()));
+    final var sw = new StringWriter();
+    final var pw = new PrintWriter(sw);
+    cause.printStackTrace(pw);
+    System.err.println(Error.prettyPrint(cause.getMessage(), sw.toString()));
     System.exit(cause.hashCode());
   }
 
   /** Format the given messages into a single string with a border around it. */
   private static String prettyPrint(final String... messages) {
-    final StringBuilder sb = new StringBuilder();
-    sb.append('\n');
-    var longestLine = 4;
-    for (final String message : messages) {
-      for (final String line : message.split("\n")) {
+    var longestLine = 0;
+    final var textBlocks = Arrays.stream(messages)
+      .map(message -> message.replaceAll("\t", "    "))
+      .collect(Collectors.toList());
+    final var sb = new StringBuilder();
+    for (final var message : textBlocks) {
+      final var lines = message.split("\n");
+      for (final var line : lines) {
         if (line.length() > longestLine) {
           longestLine = line.length();
         }
       }
     }
-    sb.append('╔');
-    for (int i = -1; i <= longestLine; ++i) {
-      sb.append('═');
+    sb.append("╔");
+    for (var i = 0; i < longestLine; ++i) {
+      sb.append("═");
     }
-    sb.append('╗');
-    sb.append('\n');
-    for (final String message : messages) {
-      for (final String line : message.split("\n")) {
-        sb.append("║ ");
+    var blockCount = 0;
+    sb.append("╗\n");
+    for (final var message : textBlocks) {
+      final var lines = message.split("\n");
+      for (final var line : lines) {
+        sb.append("║");
         sb.append(line);
-        for (int i = line.length(); i < longestLine; ++i) {
-          sb.append(' ');
+        for (var i = line.length(); i < longestLine; ++i) {
+          sb.append(" ");
         }
-        sb.append(" ║\n");
+        sb.append("║\n");
+      }
+      if (++blockCount < textBlocks.size()) {
+        sb.append("╠");
+        for (var i = 0; i < longestLine; ++i) {
+          sb.append("═");
+        }
+        sb.append("╣\n");
       }
     }
-    sb.append('╚');
-    for (int i = -1; i <= longestLine; ++i) {
-      sb.append('═');
+    sb.append("╚");
+    for (var i = 0; i < longestLine; ++i) {
+      sb.append("═");
     }
-    sb.append('╝');
-
+    sb.append("╝\n");
     return sb.toString();
   }
 
-  private Error() {}
+  private Error() {
+  }
 }
