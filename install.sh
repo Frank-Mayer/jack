a99c84e69ffc7a32a3fba2e9df93b75e07b3cfad
Frank-Mayer <git@frank-mayer.io>
2023-05-28 12:29:07 +0200

    more interactive install script

--- install.sh
+++ install.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
 
-mkdir -p ~/.jack/bin
-if mvn clean package --quiet && mv target/jack.jar ~/.jack/bin/; then
-    echo "Build succeeded."
+echo "Installing jack"
+echo "Creating directory $HOME/.jack/bin"
+mkdir -p $HOME/.jack/bin
+echo "Done"
+
+echo "Building jack from source"
+if mvn clean package --quiet && mv target/jack.jar $HOME/.jack/bin/; then
+  echo "Done"
 else
-    echo "Build failed. Downloading latest release from GitHub..."
-    curl -o ~/.jack/bin/jack.jar https://frank-mayer.github.io/jack/jack.jar
+    echo "Build failed"
+    read -r -p "Do you want to download the latest release from GitHub instead? [y/N] " response
+    case "$response" in
+      [yY][eE][sS]|[yY]) 
+        echo "Downloading latest release from GitHub"
+        curl -o $HOME/.jack/bin/jack.jar https://frank-mayer.github.io/jack/jack.jar
+        echo "Done"
+        ;;
+      *)
+        echo "Aborting"
+        echo "Installation failed"
+        exit 1
+        ;;
+    esac
 fi
 
-# jar wrapper
-cat << 'END_SCRIPT' > ~/.jack/bin/jack
+echo "Creating wrapper script for jar file"
+cat << 'END_SCRIPT' > $HOME/.jack/bin/jack
 #!/bin/bash
-java -jar ~/.jack/bin/jack.jar "$@"
+java -jar $HOME/.jack/bin/jack.jar "$@"
 END_SCRIPT
+chmod +x $HOME/.jack/bin/jack
+echo "Done"
 
-chmod +x ~/.jack/bin/jack
+command -v jack >/dev/null 2>&1 || {
+  echo "Jack is not on your PATH."
+  # find default shells rc file
+  rcfile="$HOME/.profile"
+  case "$SHELL" in
+    */zsh) rcfile="$HOME/.zshrc" ;;
+    */bash) rcfile="$HOME/.bashrc" ;;
+  esac
 
-export PATH=$PATH:~/.jack/bin
+  read -r -p "Do you want to add jack to PATH in '$rcfile'? [y/N] " response
+  case "$response" in
+    [yY][eE][sS]|[yY]) 
+      echo "" >> "$rcfile"
+      echo "# jack" >> "$rcfile"
+      echo "export PATH=\$PATH:$HOME/.jack/bin" >> "$rcfile"
+      ;;
+    *)
+      echo "You can add jack to your PATH manually by adding the following line to your rc file:"
+      echo "export PATH=\$PATH:$HOME/.jack/bin"
+      ;;
+  esac
+}
 
-echo "Make sure to add ~/.jack/bin to your PATH"
+echo "Installation complete"
