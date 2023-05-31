84eb8fd505a0a02175570d561ffb954ea54a47c2
René Ott <rene.ott1997@yahoo.de>
2023-05-31 12:54:29 +0200

    check dependencies

--- install.cmd
+++ install.cmd
@@ -18,3 +18,16 @@ echo [0m ^|                     C ^|
 echo [0m ^|                     A ^|
 echo [0m ^|                     J ^|
 echo [0m -------------------------
+
+where /q java
+IF ERRORLEVEL 1 (
+    ECHO Java is not installed or not in your PATH. Jack depends on Java being installed.
+    EXIT /B 1
+)
+
+where /q mvn
+IF ERRORLEVEL 1 (
+    ECHO Maven is not installed or not in your PATH. Jack depends on Maven being installed.
+    EXIT /B 1
+)
+
